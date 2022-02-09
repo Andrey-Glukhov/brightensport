8882601c8de493c792d65c2240fcb66644dbdb26
Andrey-Glukhov <agi2000@mail.ru>
2022-02-09 14:01:24 +0300

    change standart template

--- standard.php
+++ standard.php
@@ -5,7 +5,9 @@
 get_header(); ?>
 <div class="container-fluid standard-wrapper">
     <div class="row">
+        <?php if (get_field('opener_image')) {?>
         <div class="col-12 standard_opener" style="background-image:url(<?php the_field('opener_image')?>);"></div>
+        <?php }?>
     </div>
     <div class="standard-content-wrapper">
         <?php the_content('')?>
